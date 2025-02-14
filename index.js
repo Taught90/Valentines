06751d5da2fc57cc7eb8fac861c5b5d0bd3528c1
KELVIM MUTUKU MUTHAMA <114753155+Taught90@users.noreply.github.com>
2025-02-14 23:45:52 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const emoji1 = document.querySelector('.emoji-1');
 const emoji2 = document.querySelector('.emoji-2');
 const emoji3 = document.querySelector('.emoji-3');
 const emoji4 = document.querySelector('.emoji-4');
+const audio = document.querySelector('.audio');
 
 open.addEventListener('click', () => {
     cover.classList.toggle('opened')
@@ -20,5 +21,13 @@ open.addEventListener('click', () => {
     }
     else{
         open.innerHTML = 'Close the envelope'
+    }
+
+    if(audio.paused){
+        audio.play()
+    }
+    else{
+        audio.pause()
+        audio.currentTime = 0;
     }
 })
